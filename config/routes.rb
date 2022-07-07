68dde97ef3cea48ea269f34b33d8ff162af54c21
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-07-07 10:28:45 +0400

    delete get/post for 'contacts'

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
   resources :articles
 
   resource :contacts, only: [:new, :create]
-  get "/contacts", to: "contacts#new"
-  post "/contacts", to: "contacts#create" 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
