dff7e9008ba5e7381bd4bd4d538e619758c12841
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-07-13 12:13:01 +0400

    Add configurations for using gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'home/index'
   get 'home', to: 'home#index'
-  root 'home#index'
+  root to: "home#index"
 
   get 'terms' => 'pages#terms'
   get 'about' => 'pages#about'
