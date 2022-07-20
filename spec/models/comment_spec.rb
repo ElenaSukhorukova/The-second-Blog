5bd6809bb6b94050a216d384879c53c1476ae91a
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-07-20 09:28:17 +0400

    Add test for Comment's model. Check validation presence.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
-describe Comment do 
-  it { should belong_to :article }
-  
+describe Comment do
+
+  describe 'validations' do
+    it { should validate_presence_of :author }
+    it { should validate_presence_of :body }
+  end
+
+  describe 'associations' do
+    it { should belong_to :article }
+  end
 end
