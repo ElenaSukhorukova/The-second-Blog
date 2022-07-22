3946792a4e8b45a9a9cc0a8ce5004df8f3d5e737
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-07-22 09:08:57 +0400

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -10,4 +10,15 @@ describe Article do
   describe 'associations' do
     it { should have_many :comments }
   end
+
+  describe '#subject' do
+    it 'returns the article title' do
+      # create article's object tricky ways
+      user = create(:user,  username: 'Helen',  email: 'helen@helen.com', password: 'hello123')
+      article = create(:article, title: 'Hello', user_id: user.id)
+
+      # assert, check
+      expect(article.subject).to eq 'Hello'
+    end
+  end
 end
