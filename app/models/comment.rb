e6871cf992f9a02b5eb20608a6635b56d353bb76
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-07-22 13:01:41 +0400

    Add validation for length body of Comment and tune test for it

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :article 
+
+  validates :author, presence: true
+  validates :body, presence: true, length: { maximum: 4000 }
+
+  def body_length
+    body.length
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -10,4 +10,13 @@ describe Comment do
   describe 'associations' do
     it { should belong_to :article }
   end
+
+  describe '#length body' do
+    it 'is not more 400 items' do
+      user = create(:user,  username: 'Helen',  email: 'helen@helen.com', password: 'hello123')
+      article = create(:article_with_comment, user_id: user.id)
+
+      expect(article.comments.last.body_length).to be < 4000
+    end
+  end
 end
