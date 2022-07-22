82425959ecdd358d5daba6f50fb6b9f1e9ffbad3
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-07-22 13:15:46 +0400

    Refactoring test for article

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
-describe Article do 
-  
+describe Article do
+  let(:user) do
+    create(:user,  username: 'Helen',  email: 'helen@helen.com', password: 'hello123')
+  end
+
+  let(:article) do 
+    create(:article, title: 'Something', text: 'Something', user_id: user.id)
+  end
+
   describe 'validations' do 
     it { should validate_presence_of :title }
     it { should validate_presence_of :text }
@@ -13,32 +20,26 @@ describe Article do
 
   describe '#subject' do
     it 'returns the article title' do
-      # create article's object tricky ways
-      user = create(:user,  username: 'Helen',  email: 'helen@helen.com', password: 'hello123')
-      article = create(:article, title: 'Hello', user_id: user.id)
 
       # assert, check
-      expect(article.subject).to eq 'Hello'
+      expect(article.subject).to eq 'Something'
     end
   end
 
   describe '#last_comment' do
     it 'returns the last comment' do
-      #create user and article with comments from this user
-      user = create(:user,  username: 'Helen',  email: 'helen@helen.com', password: 'hello123')
-      article = create(:article_with_comments, user_id: user.id)
+      
+      #create article with comments from this user
+     article = create(:article_with_comments, user_id: user.id)
 
       #check
-
       expect(article.last_comment.body).to eq 'comment body 3'
     end
   end  
 
   describe '#title.length and #text.length' do
     it 'is not more 140 items and 4000 items' do
-      user = create(:user,  username: 'Helen',  email: 'helen@helen.com', password: 'hello123')
-      article = create(:article, title: 'Something', text: 'Something', user_id: user.id)
-      
+          
       expect(article.title_length).to be < 140
       expect(article.text_length).to be < 4000
     end
