c275606237f3433eabc649df88faa983106337e3
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-07-22 12:37:03 +0400

    Add validation for maximum length columns title and text of Article model. Add method for catching length.

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,8 +2,8 @@ class Article < ApplicationRecord
   has_many :comments
   belongs_to :user
   
-  validates :title, presence: true
-  validates :text, presence: true
+  validates :title, presence: true, length: { maximum: 140 }
+  validates :text, presence: true, length: { maximum: 4000 }
 
   def subject
     title
@@ -11,5 +11,10 @@ class Article < ApplicationRecord
 
   def last_comment
     comments.last
+  end
+
+  def size_title_and_text
+    title.length
+    text.length
   end
 end
