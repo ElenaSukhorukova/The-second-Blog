9f0ee0ae1f8f9017a161b66e576082a580128e80
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-07-27 14:12:21 +0400

    Create filling for form on contact's page

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -6,4 +6,15 @@ feature 'Contact creation' do
 
     expect(page).to have_content I18n.t( "contacts.contact_us")
   end
+
+  scenario "allows a guest to create contact" do
+    visit '/contacts'
+    
+      fill_in :contact_name, with: 'user@example.com'
+      fill_in :contact_email, with: 'user@example.com'
+      fill_in :contact_message, with: 'Blabla'
+
+      click_button 'Send message'
+      expect(page).to have_content 'The message successfully send'
+  end
 end
