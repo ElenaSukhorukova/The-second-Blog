cac721e7df9e04dae44b36f8103e9ff2d33cdfb5
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-07-28 15:05:02 +0400

    Add test for greeting user

--- spec/features/visitor_create_account_spec.rb
+++ spec/features/visitor_create_account_spec.rb
@@ -8,5 +8,9 @@ feature 'Account creation' do
   scenario "allows guest to create account" do
     expect(page).to have_content I18n.t('devise.registrations.signed_up')
   end
+
+  scenario 'allows to greet user with using name' do
+    expect(page).to have_content I18n.t('users.greeting_user', username: 'User')
+  end
 end
 
