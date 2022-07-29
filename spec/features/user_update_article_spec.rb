deda0d9375259356d5b0df922601e58c1d20092e
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-07-29 13:37:13 +0400

    Add test for edition an article

--- spec/features/user_update_article_spec.rb
+++ spec/features/user_update_article_spec.rb
@@ -1,19 +1,38 @@
 require 'rails_helper'
 
 feature 'Article Updating' do 
-  before(:each) do 
-    @user = User.new email: 'user@user.com', username: 'user', password: 'password', password_confirmation: 'password'
-    @article = Article.new id: 1, title: 'Any title', text: 'Any text', user_id: @user.id
+  given!(:user) { User.create(email: 'user@user.com', username: 'user', password: 'password', password_confirmation: 'password') }
+  given!(:article) { Article.create(id: 1, title: 'Any title', text: 'Any text', user_id: user.id) }
+  
+  before(:each) do
+
+    visit new_user_session_path
+
+    within("#new_user") do
+      fill_in :user_email, with: user.email
+      fill_in :user_password, with: user.password
+    end
+
+    click_button 'Log in' 
+
+    visit new_article_path
+
+    fill_in :article_title, with: article.title
+    fill_in :article_text, with: article.text
+    
+    click_button 'Send article'
   end
 
   scenario 'allows user to udate an article' do
-    visit edit_article_path(@article.id)
+    visit edit_article_path(article)
+  
+      fill_in :article_title, with: article.update(title: 'Another title')
+      fill_in :article_text, with: article.update(text: 'Another text')    
 
-    fill_in :article_title, with: 'Another title'
-    fill_in :article_text, with: 'Another text'
-
-    click_button 'Edit'
+      click_button 'Edit'
 
     expect(page).to have_content 'Article update successfully'
+    expect(article.title).to have_content 'Another title'
+
   end
 end
